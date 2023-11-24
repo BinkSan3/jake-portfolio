58050238bca0299a0f0a9a30a603f2b20c89f163
J Meyer <124314330+BinkSan3@users.noreply.github.com>
2023-11-24 11:58:19 +0000

    first commit

---
+++ src/components/projects/Projects.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Projects.css";
+
+const Projects = () => {
+  return (
+    <div className="projects">
+      <div className="project-container" id="project1"></div>
+      <div className="project-container" id="project2"></div>
+      <div className="project-container" id="project3"></div>
+      <div className="project-container" id="project4"></div>
+      <div className="project-container" id="project5"></div>
+      <div className="project-container" id="project6"></div>
+      <div className="project-container" id="project7"></div>
+    </div>
+  );
+};
+
+export default Projects;
