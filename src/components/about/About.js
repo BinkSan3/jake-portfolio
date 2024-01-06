e429aace9d058d8a467bdfb9227f0f3493659a93
J Meyer <124314330+BinkSan3@users.noreply.github.com>
2024-01-06 23:14:51 +0000

    Enhancement: Project template

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -5,14 +5,14 @@ const About = () => {
   return (
     <div className="about-container">
       <h2 className="about-title">Developer | Designer</h2>
+
       <p className="about-text">
         I am a new Full Stack Developer with a background in Graphic Art &
-        Design based in the UK. I enjoy both back end data structures and
-        architecture, and front end UX. <br></br> I always work with the user
-        experience in mind, have a passion for writing clean and reusable code,
-        whilst always looking to pass data in the most efficient way I can find.{" "}
-        <br></br>I have just completed a bootcamp in Software development and
-        seeking the next adventure.{" "}
+        Design based in the UK and have just completed a bootcamp in Software
+        development. <br></br> I always work with the user experience in mind,
+        have a passion for writing clean code and have a sensibilty in data
+        efficiency. <br></br>I spent a lot of time focused on the backend during
+        my last few projects so I hope you enjoy my Portfolio!
       </p>
     </div>
   );
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,14 +1,95 @@
 import React from "react";
 import "./Projects.css";
 import { motion, useScroll } from "framer-motion";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+
+import boxedOff1 from "../../assets/boxedOffEg.png";
+import boxedOff2 from "../../assets/boxedOffEg2.png";
+import githubwhite from "../../assets/github-mark-white.png";
+import githubblack from "../../assets/github-logo.png";
 
 const Projects = () => {
+  const [mouseHover, setMouseHover] = useState();
+  const logoBlack = () => {
+    setMouseHover(true);
+  };
+  const logoWhite = () => {
+    setMouseHover(false);
+  };
+
   return (
     <div className="projects-container">
       <h2 className="projects-title">Projects</h2>
-      <div className="project-item" id="project1"></div>
-      <div className="project-item" id="project2"></div>
+      <div className="project-item" id="project1">
+        <div className="project-text">
+          <h3>Boxed Off</h3>
+          <p>A collaborative full PERN stack Family Management app</p>
+          <h4>Features</h4>
+          <ul>
+            <li>App engaging UI/UX & Simple, consistent interface</li>
+            <li>Netflix User selection for single device use.</li>
+            <li>Hashed passwords & JWT security protected</li>
+            <li>
+              SQL database with structured data and predefined relationships
+            </li>
+            <li>Admin protected rendering for family administrator</li>
+            <li>JWT on login user and BCRYPT password hash</li>
+          </ul>
+          <div className="project-buttons">
+            <button
+              onMouseEnter={() => logoBlack()}
+              onMouseLeave={() => logoWhite()}
+              className="code-button"
+            >
+              Code{" "}
+              <img
+                src={mouseHover ? githubblack : githubwhite}
+                className="code"
+              />
+            </button>
+            <button className="launch-button">Launch Project</button>
+          </div>
+        </div>
+
+        <div className="project-images">
+          <img src={boxedOff1} className="project-image" />
+          <img src={boxedOff2} className="project-image" />
+        </div>
+      </div>
+
+      <div className="project-item" id="project2">
+        <div className="project-text">
+          <h3>Cats for Life</h3>
+          <p>A collaborative ecommerce React app</p>
+          <h4>Features</h4>
+          <ul>
+            <li>Random API generated cat ecommerce clone</li>
+            <li>Faker API genrated info tied to cat API Object</li>
+            <li>Modals</li>
+
+            <li>Admin protected rendering for family administrator</li>
+          </ul>
+          <div className="project-buttons">
+            <button
+              onMouseEnter={() => logoBlack()}
+              onMouseLeave={() => logoWhite()}
+              className="code-button"
+            >
+              Code{" "}
+              <img
+                src={mouseHover ? githubblack : githubwhite}
+                className="code"
+              />
+            </button>
+            <button className="launch-button">Launch Project</button>
+          </div>
+        </div>
+
+        <div className="project-images">
+          <img src={boxedOff1} className="project-image" />
+          <img src={boxedOff2} className="project-image" />
+        </div>
+      </div>
       <div className="project-item" id="project3"></div>
       <div className="project-item" id="project4"></div>
       <div className="project-item" id="project5"></div>
